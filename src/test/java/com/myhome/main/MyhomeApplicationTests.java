20794fbc54c5c07502510a76ff16dfffc65b8218
Milan <mvelickovic99@gmail.com>
2022-06-14 17:29:58 +0200

    V1.0
    Project base created.

---
+++ src/test/java/com/myhome/main/MyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myhome.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
